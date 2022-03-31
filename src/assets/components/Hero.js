1dc968af0e7273126a40b6ed1b367ec4d17b5d2f
Jonathan Graziani <jpgraziani@mail.com>
2022-03-30 21:36:35 -0700

    updated text

--- src/assets/components/Hero.js
+++ src/assets/components/Hero.js
@@ -4,9 +4,9 @@ import { StaticImage } from 'gatsby-plugin-image'
 const Hero = () => {
   return (
     <section className='bg-primary text-white'>
-      <div className='py-14 px-5 '>
-        <h2 className='text-2xl'>Residental & Commerical Claning Services in Tucson</h2>
-        <p className='text-base'>Locally owned and operated for over 25 years. Customers benefit from staffing continuity that reinforces residential knowledge and confidence.</p>
+      <div className='py-14 px-5'>
+        <h2 className='text-3xl font-sans leading-tight mb-5'>Residental & Commerical Claning Services in Tucson</h2>
+        <p className='text-base leading-9'>Locally owned and operated for over 25 years. Customers benefit from staffing continuity that reinforces residential knowledge and confidence.</p>
         <div className='mt-8'>
           <a className='p-3 border-2' href="tel:425.244.5326">call us now</a>
         </div>
@@ -14,13 +14,13 @@ const Hero = () => {
       
       <figure>
         <StaticImage 
-              src='../images/yehleen-gaffney-raxI_EcyfGw-unsplash.jpg'
-              alt=''
-              placeholder='blurred'
-              layout='fullWidth'
-              className=''
+          src='../images/yehleen-gaffney-raxI_EcyfGw-unsplash.jpg'
+          alt=''
+          placeholder='blurred'
+          layout='fullWidth'
+          className=''
 
-            />
+        />
       </figure>
      
     </section>
