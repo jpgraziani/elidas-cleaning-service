0177410bd4d25bd78e1396cea57244e7ba4cb368
Jonathan Graziani <jpgraziani@mail.com>
2022-04-01 09:24:13 -0700

    added container padding

--- src/assets/components/Footer.js
+++ src/assets/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='bg-blue-900 text-white'>
+    <footer className='bg-blue-900 text-white container'>
       <div>
       <h2 className='h2-title'>Elida</h2>
       <span>cleaning service</span>
@@ -10,13 +10,15 @@ const Footer = () => {
       <div className='mt-8'>
           <a className='btn' href="tel:425.244.5326">Book Cleaning</a>
         </div>
-      <ul className='mt-8 p-text'>
-        <li><a href='#'>About</a></li>
+     
+      <ul className='mt-8 p-text border-t-2 border-white'>
+        <li className='pt-8'><a href='#'>About</a></li>
         <li><a href='#'>Blog</a></li>
         <li><a href='#'>Contact</a></li>
       </ul>
-      <ul className='mt-8 p-text'>
-        <li className='pb-6'><a href='#'><div className='w-16 h-16 border-2 border-white rounded-full'></div></a></li>
+   
+      <ul className='mt-8 p-text border-t-2 border-white pb-2'>
+        <li className='pb-6 pt-8'><a href='#'><div className='w-16 h-16 border-2 border-white rounded-full'></div></a></li>
         <li className='pb-6'><a href='#'><div className='w-16 h-16 border-2 border-white rounded-full'></div></a></li>
         <li className='pb-6'><a href='#'><div className='w-16 h-16 border-2 border-white rounded-full'></div></a></li>
       </ul>
