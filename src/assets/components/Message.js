b1b616e1bfd4949b4f0e2752f6cce8b06d20e774
Jonathan Graziani <jpgraziani@mail.com>
2022-04-07 11:46:41 -0700

    updated css and form info

--- src/assets/components/Message.js
+++ src/assets/components/Message.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import FormSpree from './FormSpree'
+import badge from '../images/icon-badge.svg'
 
 const Message = () => {
   return (
     <section className='image_Bkg h-screen bg-cover bg-no-repeat bg-center flex flex-col items-center pt-6 justify-center'>
-      <span className='border-2 border-white bg-blue-500 w-16 h-16 rounded-full relative top-8 left-24'></span>
+
+     
+        <img className='w-20 h-20 relative top-8 left-24' src={badge} alt='main cleaning servies' />
+     
       <FormSpree />
     </section>
   )
